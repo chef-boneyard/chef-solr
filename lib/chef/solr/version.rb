14e4be194da33f968d471486690e660b28c93877
Seth Falcon <seth@opscode.com>
2012-12-03 14:12:24 -0800

    Move to semver pre-release version

--- lib/chef/solr/version.rb
+++ lib/chef/solr/version.rb
@@ -1,6 +1,6 @@
 class Chef
   class Solr
-    VERSION = '11.0.0.alpha'
+    VERSION = '11.0.0-alpha.1'
 
     # Solr Schema. Used to detect incompatibilities between installed solr and
     # chef-solr versions.
